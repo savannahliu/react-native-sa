c9fa481ed318d2c873382b6758d01871c434703f
savannah <savannah.m.liu.18@dartmouth.edu>
2018-05-20 21:05:38 -0400

    done

---
+++ my-new-project/youtube-api.js
@@ -0,0 +1,29 @@
+
+import axios from 'axios';
+
+const API_URL = 'https://www.googleapis.com/youtube/v3/search';
+const API_KEY = 'AIzaSyC4pteZxnX593KLR7-zF1eTVjrxZ-G5Hxo';
+// AIzaSyC4pteZxnX593KLR7-zF1eTVjrxZ-G5Hxo youtube data v3 api key
+
+const youtubeSearch = (term) => {
+  const params = {
+    part: 'snippet',
+    key: API_KEY,
+    q: term,
+    type: 'video',
+    maxResults: 15,
+  };
+
+  return new Promise((resolve, reject) => {
+    axios.get(API_URL, { params })
+      .then((response) => {
+        resolve(response.data.items);
+      })
+      .catch((error) => {
+        console.log(`youtube api error: ${error}`);
+        reject(error);
+      });
+  });
+};
+
+export default youtubeSearch;
